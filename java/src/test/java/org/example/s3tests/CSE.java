0e209bc4b3224feb7da9de39d4130db3fa4d3c5c
jwkwak <jwkwak@pspace.co.kr>
2022-03-04 18:28:47 +0900

    ssl 관련 설정 변경

--- java/src/test/java/org/example/s3tests/CSE.java
+++ java/src/test/java/org/example/s3tests/CSE.java
@@ -10,11 +10,7 @@
 */
 package org.example.s3tests;
 
-import com.amazonaws.SDKGlobalConfiguration;
-
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -22,18 +18,12 @@ public class CSE {
 
     org.example.test.CSE Test = new org.example.test.CSE();
 
-    @BeforeEach
-    public void Init() {
-        System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "true");
-    }
-
     @AfterEach
 	public void Clear() {
 		Test.Clear();
 	}
 
     @Test
-    @DisplayName("test_cse_encrypted_transfer_1b")
     @Tag("PutGet")
     // @Tag("[AES256] 1Byte 오브젝트를 암호화 하여 업로드한뒤, 다운로드하여 복호화 했을 경우 일치하는지 확인")
     public void test_cse_encrypted_transfer_1b()
@@ -42,7 +32,6 @@ public class CSE {
     }
 
     @Test
-    @DisplayName("test_cse_encrypted_transfer_1kb")
     @Tag("PutGet")
     // @Tag("[AES256] 1KB 오브젝트를 암호화 하여 업로드한뒤, 다운로드하여 복호화 했을 경우 일치하는지 확인")
     public void test_cse_encrypted_transfer_1kb()
@@ -51,7 +40,6 @@ public class CSE {
     }
 
     @Test
-    @DisplayName("test_cse_encrypted_transfer_1MB")
     @Tag("PutGet")
     // @Tag("[AES256] 1MB 오브젝트를 암호화 하여 업로드한뒤, 다운로드하여 복호화 했을 경우 일치하는지 확인")
     public void test_cse_encrypted_transfer_1MB()
@@ -60,7 +48,6 @@ public class CSE {
     }
 
     @Test
-    @DisplayName("test_cse_encrypted_transfer_13b")
     @Tag("PutGet")
     // @Tag("[AES256] 13Byte 오브젝트를 암호화 하여 업로드한뒤, 다운로드하여 복호화 했을 경우 일치하는지 확인")
     public void test_cse_encrypted_transfer_13b()
@@ -69,7 +56,6 @@ public class CSE {
     }
 
     @Test
-    @DisplayName("test_cse_encryption_method_head")
     @Tag("Metadata")
     // @Tag("[AES256] 암호화하고 메타데이터에 키값을 추가하여 업로드한 오브젝트가 올바르게 반영되었는지 확인 ")
     public void test_cse_encryption_method_head()
@@ -78,7 +64,6 @@ public class CSE {
     }
 
     @Test
-    @DisplayName("test_cse_encryption_non_decryption")
     @Tag("ERROR")
     // @Tag("[AES256] 암호화 하여 업로드한 오브젝트를 다운로드하여 비교할경우 불일치")
     public void test_cse_encryption_non_decryption()
@@ -87,7 +72,6 @@ public class CSE {
     }
 
     @Test
-    @DisplayName("test_cse_non_encryption_decryption")
     @Tag("ERROR")
     // @Tag("[AES256] 암호화 없이 업로드한 오브젝트를 다운로드하여 복호화할 경우 실패 확인")
     public void test_cse_non_encryption_decryption()
@@ -96,7 +80,6 @@ public class CSE {
     }
 
     @Test
-    @DisplayName("test_cse_encryption_range_read")
     @Tag("RangeRead")
     // @Tag("[AES256] 암호화 하여 업로드한 오브젝트에 대해 범위를 지정하여 읽기 성공")
     public void test_cse_encryption_range_read()
@@ -105,7 +88,6 @@ public class CSE {
     }
 
     @Test
-    @DisplayName("test_cse_encryption_multipart_upload")
     @Tag("Multipart")
     // @Tag("[AES256] 암호화된 오브젝트 멀티파트 업로드 / 다운로드 성공 확인")
     public void test_cse_encryption_multipart_upload()
@@ -114,7 +96,6 @@ public class CSE {
     }
 
     @Test
-    @DisplayName("test_cse_get_object_many")
     @Tag("Get")
     // @Tag("CSE설정한 오브젝트를 여러번 반복하여 다운로드 성공 확인")
     public void test_cse_get_object_many()
@@ -123,7 +104,6 @@ public class CSE {
     }
 
     @Test
-    @DisplayName("test_cse_range_object_many")
     @Tag("Get")
     // @Tag("CSE설정한 오브젝트를 여러번 반복하여 Range 다운로드 성공 확인")
     public void test_cse_range_object_many()
--- java/src/test/java/org/example/s3tests/SSE_C.java
+++ java/src/test/java/org/example/s3tests/SSE_C.java
@@ -10,12 +10,8 @@
 */
 package org.example.s3tests;
 
-import com.amazonaws.SDKGlobalConfiguration;
-
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -23,18 +19,12 @@ public class SSE_C {
 
     org.example.test.SSE_C Test = new org.example.test.SSE_C();
 
-    @BeforeEach
-    public void trustAllHosts() {
-        System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "true");
-    }
-
     @AfterEach
 	public void Clear() {
 		Test.Clear();
 	}
     
     @Test
-    @DisplayName("test_encrypted_transfer_1b")
     @Tag("PutGet")
     // @Tag("1Byte 오브젝트를 SSE-C 설정하여 업/다운로드가 올바르게 동작하는지 확인")
     public void test_encrypted_transfer_1b()
@@ -43,7 +33,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encrypted_transfer_1kb")
     @Tag("PutGet")
     // @Tag("1KB 오브젝트를 SSE-C 설정하여 업/다운로드가 올바르게 동작하는지 확인")
     public void test_encrypted_transfer_1kb()
@@ -52,7 +41,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encrypted_transfer_1MB")
     @Tag("PutGet")
     // @Tag("1MB 오브젝트를 SSE-C 설정하여 업/다운로드가 올바르게 동작하는지 확인")
     public void test_encrypted_transfer_1MB()
@@ -61,7 +49,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encrypted_transfer_13b")
     @Tag("PutGet")
     // @Tag("13Byte 오브젝트를 SSE-C 설정하여 업/다운로드가 올바르게 동작하는지 확인")
     public void test_encrypted_transfer_13b()
@@ -70,7 +57,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encryption_sse_c_method_head")
     @Tag("Metadata")
     // @Tag("SSE-C 설정하여 업로드한 오브젝트를 SSE-C 설정하여 헤더정보읽기가 가능한지 확인")
     public void test_encryption_sse_c_method_head()
@@ -79,7 +65,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encryption_sse_c_present")
     @Tag("ERROR")
     // @Tag("SSE-C 설정하여 업로드한 오브젝트를 SSE-C 설정없이 다운로드 실패 확인")
     public void test_encryption_sse_c_present()
@@ -88,7 +73,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encryption_sse_c_other_key")
     @Tag("ERROR")
     // @Tag("SSE-C 설정하여 업로드한 오브젝트와 다른 SSE-C 설정으로 다운로드 실패 확인")
     public void test_encryption_sse_c_other_key()
@@ -97,7 +81,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encryption_sse_c_invalid_md5")
     @Tag("ERROR")
     // @Tag("SSE-C 설정값중 key-md5값이 올바르지 않을 경우 업로드 실패 확인")
     public void test_encryption_sse_c_invalid_md5()
@@ -106,7 +89,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encryption_sse_c_no_md5")
     @Tag("ERROR")
     // @Tag("SSE-C 설정값중 key-md5값을 누락했을 경우 업로드 성공 확인")
     public void test_encryption_sse_c_no_md5()
@@ -115,15 +97,14 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encryption_sse_c_no_key")
     @Tag("ERROR")
     // @Tag("SSE-C 설정값중 key값을 누락했을 경우 업로드 실패 확인")
     public void test_encryption_sse_c_no_key()
     {
         Test.test_encryption_sse_c_no_key();
     }
+    
     @Test
-    @DisplayName("test_encryption_key_no_sse_c")
     @Disabled("JAVA 에서는 algorithm값을 누락해도 기본값이 지정되어 있어 에러가 발생하지 않음")
     @Tag("ERROR")
     // @Tag("SSE-C 설정값중 algorithm값을 누락했을 경우 업로드 실패 확인")
@@ -133,7 +114,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encryption_sse_c_multipart_upload")
     @Tag("Multipart")
     // @Tag("멀티파트업로드를 SSE-C 설정하여 업로드 가능 확인")
     public void test_encryption_sse_c_multipart_upload()
@@ -142,7 +122,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encryption_sse_c_multipart_bad_download")
     @Tag("Multipart")
     // @Tag("SSE-C 설정하여 멀티파트 업로드한 오브젝트와 다른 SSE-C 설정으로 다운로드 실패 확인")
     public void test_encryption_sse_c_multipart_bad_download()
@@ -151,7 +130,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encryption_sse_c_post_object_authenticated_request")
     @Tag("Post")
     // @Tag("Post 방식으로 SSE-C 설정하여 오브젝트 업로드가 올바르게 동작하는지 확인")
     public void test_encryption_sse_c_post_object_authenticated_request()
@@ -160,7 +138,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encryption_sse_c_get_object_many")
     @Tag("Get")
     // @Tag("SSE-C설정한 오브젝트를 여러번 반복하여 다운로드 성공 확인")
     public void test_encryption_sse_c_get_object_many()
@@ -169,7 +146,6 @@ public class SSE_C {
     }
 
     @Test
-    @DisplayName("test_encryption_sse_c_range_object_many")
     @Tag("Get")
     // @Tag("SSE-C설정한 오브젝트를 여러번 반복하여 Range 다운로드 성공 확인")
     public void test_encryption_sse_c_range_object_many()
@@ -179,7 +155,6 @@ public class SSE_C {
     
     
     @Test
-	@DisplayName("test_sse_c_encryption_multipart_copypart_upload") 
     @Tag( "Multipart") 
     //@Tag("SSE-C 설정하여 멀티파트로 업로드한 오브젝트를 mulitcopy 로 복사 가능한지 확인") 
     public void test_sse_c_encryption_multipart_copypart_upload()
