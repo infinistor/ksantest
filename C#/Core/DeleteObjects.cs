cb748358217b61bc916edb81a2dbfeda0ba93aa4
jwkwak <jwkwak@pspace.co.kr>
2022-06-15 19:12:03 +0900

    #18 DeleteObjects 테스트 케이스 추가

--- C#/Core/DeleteObjects.cs
+++ C#/Core/DeleteObjects.cs
@@ -9,6 +9,7 @@
 * KSAN 개발팀은 사전 공지, 허락, 동의 없이 KSAN 개발에 관련된 모든 결과물에 대한 LICENSE 방식을 변경 할 권리가 있습니다.
 */
 using System.Collections.Generic;
+using Amazon.S3;
 using Xunit;
 
 namespace s3tests
@@ -77,7 +78,41 @@ namespace s3tests
 
 			ListResponse = Client.ListObjectsV2(BucketName);
 			Assert.Empty(ListResponse.S3Objects);
+		}
 
+		[Fact(DisplayName = "test_multi_object_delete_versions")]
+		[Trait(MainData.Major, "DeleteObjects")]
+		[Trait(MainData.Minor, "Versioning")]
+		[Trait(MainData.Explanation, "버킷에 존재하는 버저닝 오브젝트 여러개를 한번에 삭제")]
+		[Trait(MainData.Result, MainData.ResultSuccess)]
+		public void test_multi_object_delete_versions()
+		{
+			var KeyNames = new List<string>() { "key0", "key1", "key2" };
+			var BucketName = GetNewBucket();
+			var Client = GetClient();
+
+			CheckConfigureVersioningRetry(BucketName, VersionStatus.Enabled);
+			foreach (var Key in KeyNames)
+				SetupMultipleVersion(Client, BucketName, Key, 3, false);
+
+			var ListResponse = Client.ListObjectsV2(BucketName);
+			Assert.Equal(KeyNames.Count, ListResponse.S3Objects.Count);
+
+			var ObjectList = GetKeyVersions(KeyNames);
+			var DelResponse = Client.DeleteObjects(BucketName, ObjectList);
+
+			Assert.Equal(KeyNames.Count, DelResponse.DeletedObjects.Count);
+			Assert.Empty(DelResponse.DeleteErrors);
+
+			ListResponse = Client.ListObjectsV2(BucketName);
+			Assert.Empty(ListResponse.S3Objects);
+
+			DelResponse = Client.DeleteObjects(BucketName, ObjectList);
+			Assert.Equal(KeyNames.Count, DelResponse.DeletedObjects.Count);
+			Assert.Empty(DelResponse.DeleteErrors);
+
+			ListResponse = Client.ListObjectsV2(BucketName);
+			Assert.Empty(ListResponse.S3Objects);
 		}
 
 		[Fact(DisplayName = "test_multi_object_delete_quiet")]
@@ -130,6 +165,45 @@ namespace s3tests
 
 			ListResponse = Client.ListObjects(BucketName);
 			Assert.Equal(4, ListResponse.S3Objects.Count);
+		}
+
+		[Fact(DisplayName = "test_directory_delete_versions")]
+		[Trait(MainData.Major, "DeleteObjects")]
+		[Trait(MainData.Minor, "Versioning")]
+		[Trait(MainData.Explanation, "버저닝 된 버킷에 업로드한 디렉토리를 삭제해도 해당 디렉토리에 오브젝트가 보이는지 확인")]
+		[Trait(MainData.Result, MainData.ResultSuccess)]
+		public void test_directory_delete_versions()
+		{
+			var KeyNames = new List<string>() { "a/", "a/obj1", "a/obj2", "b/", "b/obj1" };
+			var BucketName = GetNewBucket();
+			var Client = GetClient();
+
+			CheckConfigureVersioningRetry(BucketName, VersionStatus.Enabled);
+			foreach (var Key in KeyNames)
+				SetupMultipleVersion(Client, BucketName, Key, 3, false);
+
+			var ListResponse = Client.ListObjectsV2(BucketName);
+			Assert.Equal(KeyNames.Count, ListResponse.S3Objects.Count);
+
+			var VerResponse = Client.ListVersions(BucketName);
+			Assert.Equal(15, VerResponse.Versions.Count);
+
+			Client.DeleteObject(BucketName, "a/");
+
+			ListResponse = Client.ListObjectsV2(BucketName);
+			Assert.Equal(KeyNames.Count, ListResponse.S3Objects.Count);
+
+			VerResponse = Client.ListVersions(BucketName);
+			Assert.Equal(16, VerResponse.Versions.Count);
+
+			var DeleteList = new List<string> {"a/obj1", "a/obj2" };
+			var ObjectList = GetKeyVersions(DeleteList);
+
+			var DelResponse = Client.DeleteObjects(BucketName, ObjectList);
+			Assert.Equal(2, DelResponse.DeletedObjects.Count);
+
+			VerResponse = Client.ListVersions(BucketName);
+			Assert.Equal(18, VerResponse.Versions.Count);
 		}
 	}
 }
