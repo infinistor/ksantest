be33f4a1fe3295a839aef1d1beb830cc22c162d9
pspace-jwkwak <jwkwak@pspace.co.kr>
2024-05-27 19:00:58 +0900

    불필요한 코드 수정

--- java/src/main/java/org/example/Utility/Utils.java
+++ java/src/main/java/org/example/Utility/Utils.java
@@ -1,29 +1,35 @@
 package org.example.Utility;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
-import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Base64.Encoder;
+import java.util.Collections;
 
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
+
 public class Utils {
-	public static final char[] TEXT = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+	private Utils() {
+	}
+
+	protected static final char[] TEXT = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+			'p',
 			'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-	public static final char[] TEXT_LONG = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+	protected static final char[] TEXT_LONG = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 			'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
 			'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
 			'4', '5', '6', '7', '8', '9' };
-	public static Random rand = new Random();
 
 	public static String randomText(int length) {
-		StringBuffer sb = new StringBuffer();
+		Random rand = new Random(System.currentTimeMillis());
+		var sb = new StringBuilder();
 
 		for (int i = 0; i < length; i++)
 			sb.append(TEXT[rand.nextInt(TEXT.length)]);
@@ -31,15 +37,16 @@ public class Utils {
 	}
 
 	public static String randomTextToLong(int length) {
-		StringBuffer sb = new StringBuffer();
+		Random rand = new Random(System.currentTimeMillis());
+		var sb = new StringBuilder();
 
 		for (int i = 0; i < length; i++)
 			sb.append(TEXT_LONG[rand.nextInt(TEXT_LONG.length)]);
 		return sb.toString();
 	}
 
-	public static ArrayList<String> generateRandomString(int size, int partSize) {
-		ArrayList<String> stringList = new ArrayList<String>();
+	public static List<String> generateRandomString(int size, int partSize) {
+		var stringList = new ArrayList<String>();
 
 		int remainSize = size;
 		while (remainSize > 0) {
@@ -60,9 +67,9 @@ public class Utils {
 	public static String getMD5(String str) {
 		try {
 			MessageDigest md = MessageDigest.getInstance("MD5");
-			md.update(str.getBytes("UTF-8"));
+			md.update(str.getBytes(StandardCharsets.UTF_8));
 
-			byte byteData[] = md.digest();
+			byte[] byteData = md.digest();
 
 			Encoder encoder = Base64.getEncoder();
 			var encodeBytes = encoder.encode(byteData);
@@ -70,38 +77,32 @@ public class Utils {
 
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
 		}
 		return null;
 	}
 
-	public static ArrayList<String> getKeys(List<S3ObjectSummary> ObjectList) {
-		if (ObjectList != null) {
-			var Temp = new ArrayList<String>();
+	public static List<String> getKeys(List<S3ObjectSummary> objectList) {
+		if (objectList != null) {
+			var temp = new ArrayList<String>();
 
-			for (var S3Object : ObjectList)
-				Temp.add(S3Object.getKey());
+			for (var S3Object : objectList)
+				temp.add(S3Object.getKey());
 
-			return Temp;
+			return temp;
 		}
-		return null;
+		return Collections.emptyList();
 	}
 
-	public static boolean createDummyFile(String filePath, int size) {
-		try {
-			var file = new File(filePath);
-			file.createNewFile();
-			var fos = new FileOutputStream(file);
-			var buffer = new byte[1048576];
-			for (long i = 0; i < size; i+=buffer.length) {
-				fos.write(buffer, 0, buffer.length);
-			}
-			fos.close();
-			return true;
-		} catch (Exception e) {
+	// create dummy file
+	public static void createDummyFile(String filePath, int size) {
+		Random rand = new Random(System.currentTimeMillis());
+		byte[] bytes = new byte[size];
+		rand.nextBytes(bytes);
+
+		try (FileOutputStream fos = new FileOutputStream(filePath)) {
+			fos.write(bytes);
+		} catch (IOException e) {
 			e.printStackTrace();
-			return false;
 		}
 	}
 }
