5cabbe148d72a02e445e0d5ab1312570ca03987b
jwkwak <jwkwak@pspace.co.kr>
2026-01-07 18:52:28 +0900

    불필요한 파일 삭제 및 업로드 방지

--- Utils/xmlParser/run.ps1
+++
@@ -1,3 +0,0 @@
-clear
-mvn clean package
-java -jar target/xmlReportParser -f "2025-05-28_10-45_Manual.xml"
--- Utils/xmlParser/upload.ps1
+++
@@ -1,3 +0,0 @@
-clear
-mvn clean package
-scp target/xmlReportParser root@192.168.11.245:/DOCKER/utils/xml_report_parser/
