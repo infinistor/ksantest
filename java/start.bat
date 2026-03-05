3fec687e84eac8fd2bd090e2a3499fa4ea89373e
jwkwak <jwkwak@pspace.co.kr>
2026-03-05 10:20:33 +0900

    스크립트 실행 방법 변경

--- java/start.bat
+++ java/start.bat
@@ -8,7 +8,7 @@ cls
 del xunit-to-html-master\Result_java.html
 del xunit-to-html-master\Result_java.xml
 call mvn clean
-call mvn test surefire-report:report -Ds3tests.ini=%INI_FILE%
+call mvn test surefire-report:report "-Ds3tests.ini=%INI_FILE%"
 python merge_junit_results.py target\results\*.xml > xunit-to-html-master\Result_java.xml
 cd xunit-to-html-master
 java -jar saxon9he.jar -o:Result_java.html -s:Result_java.xml -xsl:xunit_to_html.xsl
--- java/start.sh
+++ java/start.sh
@@ -1,7 +1,13 @@
 clear
-export S3TESTS_INI=config.ini
+
+if [ "$1" != "" ]; then
+    INI_FILE="$1.ini"
+else
+    INI_FILE="config.ini"
+fi
+
 mvn clean
-mvn test surefire-report:report
+mvn test surefire-report:report "-Ds3tests.ini=$INI_FILE"
 python ./merge_junit_results.py ./target/results/*.xml > xunit-to-html-master/Result_java.xml
 cd xunit-to-html-master
 java -jar saxon9he.jar -o:Result_java.html -s:Result_java.xml -xsl:xunit_to_html.xsl
